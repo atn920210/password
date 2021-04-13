dcdc08c5159f0f15b57364e61b8012735ad461b8
Austin PC Chen <atn920210@gmail.com>
2021-04-14 00:17:04 +0800

    update code

--- password.py
+++ password.py
@@ -1,10 +1,11 @@
+password = 'a123456'
 x = 0
 while x < 3:
-	password = input('請輸入密碼:')
-	if password !='a123456':
+	pwd = input('請輸入密碼:')
+	if pwd != password:
 		print('密碼錯誤!','還剩下',2-x,'次機會')
 		x = x + 1
-	elif password =='a123456':
+	elif pwd == password:
 		print('登入成功!')
 		break
 
